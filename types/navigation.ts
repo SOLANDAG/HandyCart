2b252145bfa174f7a42f5e8c1f2f3787b2cc20bf
nehpetse <nicolasnehpets@gmail.com>
2025-07-18 18:40:04 +0800

    Update navigation.ts

--- types/navigation.ts
+++ types/navigation.ts
@@ -21,7 +21,7 @@ export type DrawerParamList = {
   Babycare: undefined;
   Petcare: undefined;
   Pantrystaples: undefined;
-  
+
   ///// FOOTER /////
   Order: undefined;
   Cart: undefined;
@@ -41,47 +41,53 @@ export type DrawerParamList = {
   Login: undefined;
   Logout: undefined;
   Register: undefined;
+
+  ///// OTP SCREEN /////
+  OTP: { otp: string };
 };
 
 ///// HEADER /////
-export type FavoritesStackParamList = { FavoritesMain: undefined; };
+export type FavoritesStackParamList = { FavoritesMain: undefined };
 
 ///// GROCERY ITEMS /////
-export type FruitsStackParamList = { FruitsMain: undefined; };
-export type VegetablesStackParamList = { VegetablesMain: undefined; };
-export type CannedgoodsStackParamList = { CannedgoodsMain: undefined; };
-export type DairyStackParamList = { DairyMain: undefined; };
-export type MeatStackParamList = { MeatMain: undefined; };
-export type SeafoodStackParamList = { SeafoodMain: undefined; };
-export type DeliStackParamList = { DeliMain: undefined; };
-export type CondimentsStackParamList = { CondimentsMain: undefined; };
-export type SnacksStackParamList = { SnacksMain: undefined; };
-export type BakedgoodsStackParamList = { BakedgoodsMain: undefined; };
-export type BeveragesStackParamList = { BeveragesMain: undefined; };
-export type GrainsStackParamList = { GrainsMain: undefined; };
-export type HygieneStackParamList = { HygieneMain: undefined; };
-export type HouseholdStackParamList = { HouseholdMain: undefined; };
-export type HealthcareStackParamList = { HealthcareMain: undefined; };
-export type BabycareStackParamList = { BabycareMain: undefined; };
-export type PetcareStackParamList = { PetcareMain: undefined; };
-export type PantrystaplesStackParamList = { PantrystaplesMain: undefined; };
+export type FruitsStackParamList = { FruitsMain: undefined };
+export type VegetablesStackParamList = { VegetablesMain: undefined };
+export type CannedgoodsStackParamList = { CannedgoodsMain: undefined };
+export type DairyStackParamList = { DairyMain: undefined };
+export type MeatStackParamList = { MeatMain: undefined };
+export type SeafoodStackParamList = { SeafoodMain: undefined };
+export type DeliStackParamList = { DeliMain: undefined };
+export type CondimentsStackParamList = { CondimentsMain: undefined };
+export type SnacksStackParamList = { SnacksMain: undefined };
+export type BakedgoodsStackParamList = { BakedgoodsMain: undefined };
+export type BeveragesStackParamList = { BeveragesMain: undefined };
+export type GrainsStackParamList = { GrainsMain: undefined };
+export type HygieneStackParamList = { HygieneMain: undefined };
+export type HouseholdStackParamList = { HouseholdMain: undefined };
+export type HealthcareStackParamList = { HealthcareMain: undefined };
+export type BabycareStackParamList = { BabycareMain: undefined };
+export type PetcareStackParamList = { PetcareMain: undefined };
+export type PantrystaplesStackParamList = { PantrystaplesMain: undefined };
 
 ///// FOOTER /////
-export type OrderStackParamList = { OrderMain: undefined; };
-export type CartStackParamList = { CartMain: undefined; };
-export type HomeStackParamList = { HomeMain: undefined; };
-export type ChatsStackParamList = { ChatsMain: undefined; };
-export type EmergencyStackParamList = { EmergencyMain: undefined; };
+export type OrderStackParamList = { OrderMain: undefined };
+export type CartStackParamList = { CartMain: undefined };
+export type HomeStackParamList = { HomeMain: undefined };
+export type ChatsStackParamList = { ChatsMain: undefined };
+export type EmergencyStackParamList = { EmergencyMain: undefined };
 
 ///// SIDEBAR /////
-export type ProfileStackParamList = { ProfileMain: undefined; };
-export type SettingsStackParamList = { SettingsMain: undefined; };
-export type HistoryStackParamList = { HistoryMain: undefined; };
-export type PaymentStackParamList = { PaymentMain: undefined; };
-export type AboutStackParamList = { AboutMain: undefined; };
-export type HelpStackParamList = { HelpMain: undefined; };
+export type ProfileStackParamList = { ProfileMain: undefined };
+export type SettingsStackParamList = { SettingsMain: undefined };
+export type HistoryStackParamList = { HistoryMain: undefined };
+export type PaymentStackParamList = { PaymentMain: undefined };
+export type AboutStackParamList = { AboutMain: undefined };
+export type HelpStackParamList = { HelpMain: undefined };
 
 ///// USER AUTHENTICATION /////
-export type LoginStackParamList = { LoginMain: undefined; };
-export type LogoutStackParamList = { LogoutMain: undefined; };
-export type RegisterStackParamList = { RegisterMain: undefined; };
+export type LoginStackParamList = { LoginMain: undefined };
+export type LogoutStackParamList = { LogoutMain: undefined };
+export type RegisterStackParamList = { RegisterMain: undefined };
+
+///// OTP SCREEN /////
+export type OTPStackParamList = { OTPMain: { otp: string } };
